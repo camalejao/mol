9b7381893b547c20cb838f2e852296b4d8da14a0
Joao Falcao <joao.falcao@icloud.com>
2018-07-05 00:09:54 -0300

    home aluno (atividades)

--- src/mol/controller/AlunoController.java
+++ src/mol/controller/AlunoController.java
@@ -1,13 +1,79 @@
 package mol.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import mol.dao.DAOFactory;
+import mol.dao.IAtividadeDAO;
+import mol.dao.IRespostaDAO;
+import mol.dao.ITurmaDisciplinaAlunoDAO;
+import mol.model.StatusEntidade;
+import mol.model.curso.turma.Atividade;
+import mol.model.curso.turma.Resposta;
+import mol.model.curso.turma.TurmaDisciplinaAluno;
+import mol.model.user.Aluno;
+import mol.model.user.Usuario;
 
 @Controller
 public class AlunoController {
-	
+
 	@RequestMapping("homeAluno")
-	public String inicio() {
-        return "aluno/index";
-    }
+	public ModelAndView inicio(HttpSession session) {
+		ModelAndView mav = new ModelAndView("aluno/index");
+
+		Aluno aluno = (Aluno) session.getAttribute("usuarioLogado");
+
+		ITurmaDisciplinaAlunoDAO tdaDAO = DAOFactory.getTurmaDisciplinaAlunoDAO();
+		IAtividadeDAO atvDAO = DAOFactory.getAtividadeDAO();
+		List<Atividade> listAtv = new ArrayList<>();
+		List<TurmaDisciplinaAluno> turmasDisc = tdaDAO.consultarPorAluno(aluno);
+		
+		for (TurmaDisciplinaAluno tda : turmasDisc) {
+			List<Atividade> la = atvDAO.consultarPorTurmaDisciplina(tda.getTurmaDisciplina());
+			if (!la.isEmpty()) {
+				listAtv.addAll(la);
+			}
+		}
+
+		mav.addObject("turmasDisc", turmasDisc);
+		mav.addObject("atividades", listAtv);
+
+		return mav;
+	}
+	
+	@RequestMapping("responderAtividade-{id}")
+	public ModelAndView responder(@PathVariable Integer id, HttpSession session) {
+		ModelAndView mav = new ModelAndView("aluno/submeterResposta");
+		IAtividadeDAO atvDAO = DAOFactory.getAtividadeDAO();
+		session.setAttribute("idAtv", id);
+		mav.addObject("atividade", atvDAO.consultarPorId(id));
+		return mav;
+	}
+	
+	@RequestMapping("enviarResposta")
+	public String enviar(Resposta resposta, HttpSession session) {
+		
+		Aluno a = (Aluno)session.getAttribute("usuarioLogado");
+		Integer id = (Integer)session.getAttribute("idAtv");
+		session.removeAttribute("idAtv");
+		
+		IRespostaDAO rDAO = DAOFactory.getRespostaDAO();
+		IAtividadeDAO atvDAO = DAOFactory.getAtividadeDAO();
+		
+		resposta.setAluno(a);
+		resposta.setAtividade(atvDAO.consultarPorId(id));
+		resposta.setStatus(StatusEntidade.ATIVO);
+		resposta.setUsuarioLogado(a);
+		
+		rDAO.inserir(resposta);
+		
+		return "redirect:index";
+	}
 }
--- src/mol/dao/TurmaDisciplinaAlunoDAO.java
+++ src/mol/dao/TurmaDisciplinaAlunoDAO.java
@@ -1,8 +1,12 @@
 package mol.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import mol.model.curso.turma.TurmaDisciplinaAluno;
+import mol.model.user.Aluno;
 
 public class TurmaDisciplinaAlunoDAO extends DAOGenerico<TurmaDisciplinaAluno>
 		implements ITurmaDisciplinaAlunoDAO {
@@ -10,5 +14,17 @@ public class TurmaDisciplinaAlunoDAO extends DAOGenerico<TurmaDisciplinaAluno>
 	public TurmaDisciplinaAlunoDAO(EntityManager em) {
 		super(em);
 	}
-
+	
+	@Override
+	public List<TurmaDisciplinaAluno> consultarPorAluno(Aluno a) {
+		try {
+			TypedQuery<TurmaDisciplinaAluno> query = getEntityManager().createQuery("select tda from TurmaDisciplinaAluno tda where tda.aluno = :aluno", TurmaDisciplinaAluno.class);
+			query.setParameter("aluno", a);
+            return query.getResultList();
+            		
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        }
+		return null;
+	}
 }
