4cd223ec7d7a5d1535ede3aa24f8e82dee673d06
Joao Falcao <joao.falcao@icloud.com>
2018-09-23 00:25:38 -0300

    parametros publicos

--- src/mol/util/ParametrosGlobais.java
+++ src/mol/util/ParametrosGlobais.java
@@ -2,11 +2,6 @@ package mol.util;
 
 public class ParametrosGlobais {
 	
-	private static final Integer PERCENTUAL_APRENDIZAGEM_MINIMO = 75;
+	public static final Integer PERCENTUAL_APRENDIZAGEM_MINIMO = 75;
 	
-	
-	
-	public static Integer getPercentual() {
-		return PERCENTUAL_APRENDIZAGEM_MINIMO;
-	}
 }
