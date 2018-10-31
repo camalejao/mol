99a62163168ac2267ad95db9d5a358fa9b87c95f
Joao Falcao <joao.falcao@icloud.com>
2018-10-30 23:04:26 -0300

    campo texto na classe resposta duvida

--- src/mol/model/curso/atividade/duvida/RespostaDuvida.java
+++ src/mol/model/curso/atividade/duvida/RespostaDuvida.java
@@ -1,8 +1,13 @@
 package mol.model.curso.atividade.duvida;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import mol.model.Entidade;
 import mol.model.user.Usuario;
@@ -10,6 +15,11 @@ import mol.model.user.Usuario;
 @Entity
 @Table(name="t_resp_duvidas")
 public class RespostaDuvida extends Entidade{
+	
+	@NotEmpty @NotBlank
+	@Size(max=400)
+	@Column(length = 400, nullable = false)
+	private String resposta;
 	
 	@ManyToOne
 	private Usuario autor;
@@ -19,6 +29,14 @@ public class RespostaDuvida extends Entidade{
 
 	
 	
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+
 	public Usuario getAutor() {
 		return autor;
 	}
